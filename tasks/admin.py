147680f1686e0903a24b5cfd819e14d37f2dd08d
Princess Heaven Rica <ricaheaven18@email.com>
2026-03-20 19:57:40 +0800

    Added models and admin setup

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Task, SubTask, Category, Priority, Note
+
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Category)
+admin.site.register(Priority)
+admin.site.register(Note)
